c0a2e557831b27b80654eb78fdb4da679d6339b1
Anastasija <anastasijamijaljevic11@gmail.com>
2023-06-26 15:48:14 +0200

    Radnici models

--- Inventar/Models/Radnici.cs
+++ Inventar/Models/Radnici.cs
@@ -1,15 +1,32 @@
-﻿namespace Inventar.Models
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+
+namespace Inventar.Models
 {
+    [BsonIgnoreExtraElements]
     public class Radnici
     {
-        public Guid Id { get; set; }
-        
-        public string JMBG { get; set; }
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = String.Empty;
 
-        public string Ime { get; set; }
-        public string Prezime { get; set; }
-        public string Pol { get;set; }
-        public string Sprema { get; set; }
+        [BsonElement("jbmg")]
+
+        public string JMBG { get; set; } = String.Empty;
+
+        [BsonElement("name")]
+
+        public string Ime { get; set; } = String.Empty;
+
+        [BsonElement("lastname")]
+        public string Prezime { get; set; } = String.Empty;
+
+        [BsonElement("gender")]
+        public string Pol { get;set; } = String.Empty;
+
+        [BsonElement("qualifications")]
+        public string Sprema { get; set; } = String.Empty;
 
 
      }
